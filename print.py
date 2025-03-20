1b41fc27c8b7ea6c64270908893b2a1359b27e58
Athulya Esther <126341347+athulyaesther777@users.noreply.github.com>
2025-03-20 16:27:04 +0530

    Add files via upload

---
+++ print.py
@@ -0,0 +1,26 @@
+# direct variable printing
+fruit = 'apple' # define a variable
+#direct print
+print(fruit)
+
+num = 7
+
+# Concatenation printing
+print("My favorite fruit is " + fruit)
+
+
+# printing using comma separation
+print("My favorite fruit is",fruit)
+print("My favorite number is", num)
+
+# using format() method
+print("My favorite fruit is {}".format(fruit))
+
+# f string printing
+print(f"My fav num is {num} and my fav fruit is {fruit}")
+
+# using sep and end parameters
+print("Apple","Banana", "cherry", sep = "-")
+
+print("this line one ",end = "")
+print("this is line two.")
