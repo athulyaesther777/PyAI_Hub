adc1799b65b4c922febd3f2766d27f0d721acc69
Athulya Esther <126341347+athulyaesther777@users.noreply.github.com>
2025-09-05 21:14:00 +0530

    Create bullet.py

---
+++ Project Idea: Space Defender (Python)/bullet.py
@@ -0,0 +1,13 @@
+import pygame
+from settings import *
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super().__init__()
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def update(self):
+        self.rect.y -= BULLET_SPEED
+        if self.rect.bottom < 0:
+            self.kill()
