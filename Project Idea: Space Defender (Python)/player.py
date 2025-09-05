4bb818f7c1d46c4ff1245f26b1d1656e51e0a11d
Athulya Esther <126341347+athulyaesther777@users.noreply.github.com>
2025-09-05 21:13:40 +0530

    Create player.py

---
+++ Project Idea: Space Defender (Python)/player.py
@@ -0,0 +1,14 @@
+import pygame
+from settings import *
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, image):
+        super().__init__()
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect(center=(x, y))
+
+    def move(self, keys):
+        if keys[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.x -= PLAYER_SPEED
+        if keys[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
+            self.rect.x += PLAYER_SPEED
