a9a49f21641da825c8ed8e8887ee1db972953139
Athulya Esther <126341347+athulyaesther777@users.noreply.github.com>
2026-02-14 18:21:59 +0530

    Update and rename 01_database_thinking.sql to 01_why_sql_for_data_analysis.sql

---
+++ SQL_Course/Level_1_Basics/01_why_sql_for_data_analysis.sql
@@ -0,0 +1,14 @@
+
+PS C:\Users\athul> duckdb
+DuckDB v1.4.4 (Andium) 6ddac802ff
+Enter ".help" for usage hints.
+Connected to a transient in-memory database.
+Use ".open FILENAME" to reopen on a persistent database.
+D SELECT 'SQL is powerful' AS message;
+┌─────────────────┐
+│     message     │
+│     varchar     │
+├─────────────────┤
+│ SQL is powerful │
+└─────────────────┘
+D
